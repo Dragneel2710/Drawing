cbff11cfc07827259ee8da29b1f2e8139d3b3f0a
Dragneel2710 <61239842+Dragneel2710@users.noreply.github.com>
2020-06-19 20:03:17 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,16 @@
 var colour,colourI;
 
+var positions = [];
+
+var database;
+
 function setup() {
   createCanvas(500,500);
   colour = createSprite(250, 250, 20, 20);
   colour.addImage(colourI);
   colour.scale = 0.3;
+
+  database = firebase.database()
 }
 
 function preload(){
@@ -20,5 +26,18 @@ function Drawing(){
   if(mousePressedOver(colour)){
     colour.x = World.mouseX;
     colour.y = World.mouseY;
+    for (var i  = 0 ; i < positions.length; i++){
+      database.ref('Positions').set({
+        Positions : positions
+      })
+    }
   }
+}
+
+function mouseDragged(){
+  var points = {
+    x : mouseX, y : mouseY
+  }
+  positions.push(points)
+  console.log(positions);
 }
